c050065e013ce615ab2627cf6da78bf07b84a13e
Colin McMahon <cmcmahon1012@gmail.com>
2019-08-23 17:41:26 -0400

    added friends data to the friends js file

---
+++ app/data/friends.js
@@ -0,0 +1,35 @@
+var friendsArray = [
+    {
+        name: "Odin: The Allfather",
+        tribe: "Aesir",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/f/f8/Odin%2C_der_G%C3%B6ttervater.jpg",
+        scores: [1, 2, 3, 4, 5, 5, 4, 3, 2, 1]
+    },
+    {
+        name: "Thor: God of Thunder",
+        tribe: "Aesir",
+        photo: "https://i.imgur.com/tpmCFyl.jpg",
+        scores: [1, 2, 3, 4, 5, 5, 4, 3, 2, 1]
+    },
+    {
+        name: "Freyja: Goddess of Love",
+        tribe: "Vanir",
+        photo: "https://norse-mythology.org/wp-content/uploads/2012/11/freyja3.jpg",
+        scores: [1, 2, 3, 4, 5, 5, 4, 3, 2, 1]
+    },
+    {
+        name: "Loki: Son of Farbauti & God of Mischeif",
+        photo: "http://www.germanicmythology.com/works/JOHNSONART_files/LOKI.jpg",
+        tribe: "Jötunns",
+        scores: [1, 2, 3, 4, 5, 5, 4, 3, 2, 1]
+    },
+    {
+        name: "Hel: Daughter of Loki & Goddess of Death",
+        tribe: "Aesir",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/f/f8/Odin%2C_der_G%C3%B6ttervater.jpg",
+        scores: [1, 2, 3, 4, 5, 5, 4, 3, 2, 1]
+    },
+];
+
+module.exports = friendsArray;
+
