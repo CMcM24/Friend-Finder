02eaa66e73e8a53cf633d43bc39c07e420b82320
Colin McMahon <cmcmahon1012@gmail.com>
2019-08-23 17:12:12 -0400

    added html and api routing to the routing js files

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,17 @@
+
+//get route to the /survey page ('/survey')
+
+//a default 'catch all' route that directs to the home.html file ('/')
+
+
+var path = require("path");
+
+module.exports = function(app){
+    app.get("*", function(req, res){
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+
+    app.get("/survey", function(req, res){
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
+};
