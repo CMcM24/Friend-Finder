0f1d051420aa33f412ec5d26a8e158835bb99604
Colin McMahon <cmcmahon1012@gmail.com>
2019-08-25 00:29:02 -0400

    all changes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -11,6 +11,9 @@ module.exports = function(app){
         res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 
+    app.get("/", function(req, res) {
+        res.json(path.join(__dirname, "../public/home.html"));
+
     app.get("/survey", function(req, res){
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
