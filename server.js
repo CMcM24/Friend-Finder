5f9b094b6ccd81b1cc01533ee91a99635fc69340
Colin McMahon <cmcmahon1012@gmail.com>
2019-08-24 23:53:53 -0400

    added all html elements and some more JS to the porject

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 // Dependencies
 // =============================================================
 var express = require("express");
-var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
@@ -9,4 +8,13 @@ var PORT = 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
 
