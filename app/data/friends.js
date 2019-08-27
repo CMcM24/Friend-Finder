e54799a793534120ad8f547137809551b621a8f4
Colin McMahon <cmcmahon1012@gmail.com>
2019-08-27 17:54:39 -0400

    fixed friend calculation. rewrote script to determine friend with lowest diff in answers

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,27 +2,27 @@ var friendsArray = [
     {
         name: "Odin: The Allfather",
         photo: "https://upload.wikimedia.org/wikipedia/commons/f/f8/Odin%2C_der_G%C3%B6ttervater.jpg",
-        scores: [1, 2, 3, 4, 5, 5, 4, 3, 2, 1]
+        scores: [4, 3, 2, 4, 5, 5, 4, 3, 4, 2]
     },
     {
         name: "Thor: God of Thunder",
         photo: "https://i.imgur.com/tpmCFyl.jpg",
-        scores: [1, 2, 3, 4, 5, 5, 4, 3, 2, 1]
+        scores: [4, 4, 3, 4, 5, 3, 4, 5, 4, 5]
     },
     {
         name: "Freyja: Goddess of Love",
         photo: "https://norse-mythology.org/wp-content/uploads/2012/11/freyja3.jpg",
-        scores: [1, 2, 3, 4, 5, 5, 4, 3, 2, 1]
+        scores: [2, 5, 5, 4, 2, 5, 4, 5, 4, 1]
     },
     {
         name: "Loki: Son of Farbauti & God of Mischeif",
         photo: "http://www.germanicmythology.com/works/JOHNSONART_files/LOKI.jpg",
-        scores: [1, 2, 3, 4, 5, 5, 4, 3, 2, 1]
+        scores: [5, 2, 4, 4, 2, 5, 4, 2, 3, 1]
     },
     {
         name: "Hel: Daughter of Loki & Goddess of Death",
         photo: "https://upload.wikimedia.org/wikipedia/commons/f/f8/Odin%2C_der_G%C3%B6ttervater.jpg",
-        scores: [1, 2, 3, 4, 5, 5, 4, 3, 2, 1]
+        scores: [5, 1, 2, 4, 2, 2, 4, 1, 1, 1]
     },
 ];
 
